67868fbf47b553efb9c2588a477a2ceda33f93ea
Firel33t!! <dj@deege.com>
2014-05-10 22:44:03 -0600

    intial checkin

---
+++ src/main/java/com/deege/protocols/exceptions/ItemNotFoundException.java
@@ -0,0 +1,10 @@
+package com.deege.protocols.exceptions;
+
+public class ItemNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7584214021589976232L;
+
+}
