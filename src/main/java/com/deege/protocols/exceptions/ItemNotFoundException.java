e3f46d0e6b4313f612a46c6d8428168939ff3232
Firel33t!! <dj@deege.com>
2014-05-23 19:41:36 -0600

    Added datastore functionality to application

--- src/main/java/com/deege/protocols/exceptions/ItemNotFoundException.java
+++ src/main/java/com/deege/protocols/exceptions/ItemNotFoundException.java
@@ -1,10 +1,17 @@
 package com.deege.protocols.exceptions;
 
-public class ItemNotFoundException extends Exception {
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class ItemNotFoundException extends WebApplicationException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7584214021589976232L;
-
+	
+	public ItemNotFoundException(String message) {	
+		super(Response.status(404).entity(message).type(MediaType.TEXT_PLAIN).build());
+	}
 }
